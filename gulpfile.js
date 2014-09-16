35a44d088e439f8260b6aff3c986940d7b0d6994
Liam Curry <liam@curry.name>
2014-09-16 13:29:38 -0600

    added sass files + compass support in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,19 @@ var size = require('gulp-filesize')
 var concat = require('gulp-concat')
 var uglify = require('gulp-uglify')
 var rename = require('gulp-rename')
+var compass = require('gulp-compass')
 var browserify = require('gulp-browserify')
 var jsifyTemplates = require('gulp-jsify-html-templates')
 
 gulp.task('html', function () {
-	return gulp.src('src/templates/*.html')
+	return gulp.src('html/**/*.html')
 		.pipe(jsifyTemplates())
 		.pipe(concat('html.js'))
 		.pipe(gulp.dest('build'))
 })
 
 gulp.task('browserify', function () {
-	return gulp.src('src/main.js')
+	return gulp.src('js/main.js')
 		.pipe(browserify())
 		.pipe(gulp.dest('build'))
 })
@@ -31,17 +32,23 @@ gulp.task('js', ['browserify', 'html'], function () {
 })
 
 gulp.task('css', function () {
-	return gulp.src('src/*.css')
-		.pipe(myth())
+	return gulp.src('css/**/*.scss')
+		.pipe(compass({
+			sass: 'css',
+			css: 'build',
+			require: ['sass-globbing']
+		}))
+		.pipe(rename('nio.css'))
+		.pipe(gulp.dest('.'))
 		.pipe(csso())
 		.pipe(rename('nio.min.css'))
 		.pipe(gulp.dest('.'))
 })
 
 gulp.task('watch', function () {
-	gulp.watch('src/*.html', ['js'])
-	gulp.watch('src/*.css', ['css'])
-	gulp.watch('src/**/*.js', ['js'])
+	gulp.watch('html/**/*.html', ['js'])
+	gulp.watch('css/**/*.scss', ['css'])
+	gulp.watch('js/**/*.js', ['js'])
 })
 
 gulp.task('build', ['css', 'js'])
