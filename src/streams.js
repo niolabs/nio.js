574a81f75af1f3746f3d602a81fcde9673aa2f8f
Chris Brackert <cbrackert@gmail.com>
2014-12-15 15:48:07 -0800

    Adds 'is' pipe that allows chunks where property has a specified value.

--- src/streams.js
+++ src/streams.js
@@ -127,6 +127,19 @@ exports.filter = function (fn) {
 }
 
 /**
+ * filter to push chunk if property's value is value.
+ *
+ * @param property
+ * @param value
+ * @return {stream}
+ */
+exports.is = function (property, value) {
+    return exports.filter(function(d) {
+        return d[property] == value
+    })
+}
+
+/**
  * unique IDs chunks that are sent to it and only pushes ones it hasn't seen
  * yet.
  *
