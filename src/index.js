6aa0cab9a68d8af53b2f9499f308fec83ecd130b
mattdodge <matt@n.io>
2015-04-16 14:38:38 -0700

    Upgrading to latest version of lodash

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-var _ = require('lodash')
+var _ = require('lodash-node')
 
 module.exports = window.nio = _.assign(
 	{
 		// exposing various dependencies
 		d3: require('d3'),
-		_: require('lodash'),
+		_: require('lodash-node'),
 
 		// our modules
 		stream: require('./stream'),
